882eb4737a7ed565fa3520a270c3cc0ba73faf29
tysker <oerteljoerg@gmail.com>
2020-11-22 12:58:07 +0100

    added groovy file

--- script.groovy
+++ script.groovy
@@ -19,4 +19,14 @@ def deployProject() {
     echo "deploying version ${params.VERSION}"
 }
 
+def para() {
+    [
+    string(name: 'BUILD_REASON', defaultValue: '', description: 'commit message....'),
+    choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: ''),
+    booleanParam(name: 'executeUnitTests', defaultValue: true, description: ''),
+    booleanParam(name: 'executeIntegrationTests', defaultValue: false, description: '')
+    ]
+
+}
+
 return this 
