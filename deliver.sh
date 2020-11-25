4cb8bfa6de466f62ee4c74c718df8252583e6640
tysker <oerteljoerg@gmail.com>
2020-11-25 19:38:39 +0100

    added

--- deliver.sh
+++ deliver.sh
@@ -29,10 +29,10 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 
-# java -jar target/${NAME}-${VERSION}.jar
 
 # touch text.txt 
 # echo 'asdfsd' > text.txt
 # scp ssh -rv text.txt root@206.81.29.87:/
 
-su scp ssh target/${NAME}-${VERSION}.jar root@206.81.29.87:/
+scp ssh target/${NAME}-${VERSION}.jar root@206.81.29.87:/
+# java -jar target/${NAME}-${VERSION}.jar
