6482bba312a8e42c86ef10c7d86a321e0a868074
tysker <oerteljoerg@gmail.com>
2020-11-26 11:37:00 +0100

    added

--- src/main/java/dk/lsd/Main.java
+++ src/main/java/dk/lsd/Main.java
@@ -3,8 +3,12 @@ package dk.lsd;
 
 public class Main {
 
-
     public String getString(){
         return "Hello World";
     }
+
+    public static void main(String[] args) {
+        System.out.println("I hope it works!!!!!!");
+    }
 }
+    
