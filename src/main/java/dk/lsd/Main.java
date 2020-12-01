b9f47256b7c639e72738c7161e92a8a88f1e3291
tysker <oerteljoerg@gmail.com>
2020-12-01 11:27:17 +0100

    changed in jenkisfile

--- src/main/java/dk/lsd/Main.java
+++ src/main/java/dk/lsd/Main.java
@@ -3,40 +3,40 @@ package dk.lsd;
 import dk.lsd.rmi.RMIMethodsImpl;
 import org.apache.log4j.Logger;
 
-
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Main {
-    private final static Logger log =  Logger.getLogger(Main.class.getName());
-    public String getString(){
+    //private final static Logger log =  Logger.getLogger(Main.class.getName());
+    public static String getString(){
         return "Hello World";
     }
 
     public static Registry registry;
 
     public static void main(String[] args) {
-        try
-        {
-            log.info("INFO LOG");
-            log.error("ERROR LOG");
-
-            // Create a server registry at default port 1099
-            registry = LocateRegistry.createRegistry(1099);
-
-            // Create engine of remote methods, running on the server
-            RMIMethodsImpl remoteMethods = new RMIMethodsImpl();
-
-            // Give a name to this engine
-            String engineName = "moonlodge";
-
-            // Register the engine by the name, which later will be given to the clients
-            Naming.rebind("//localhost/" + engineName, remoteMethods);
-        }
-        catch (Exception e)
-        {
-            System.err.println("Exception:" + e);
-        }
+        System.out.println(getString());
+//        try
+//        {
+//            log.info("INFO LOG");
+//            log.error("ERROR LOG");
+//
+//            // Create a server registry at default port 1099
+//            registry = LocateRegistry.createRegistry(1099);
+//
+//            // Create engine of remote methods, running on the server
+//            RMIMethodsImpl remoteMethods = new RMIMethodsImpl();
+//
+//            // Give a name to this engine
+//            String engineName = "moonlodge";
+//
+//            // Register the engine by the name, which later will be given to the clients
+//            Naming.rebind("//localhost/" + engineName, remoteMethods);
+//        }
+//        catch (Exception e)
+//        {
+//            System.err.println("Exception:" + e);
+//        }
     }
 }
